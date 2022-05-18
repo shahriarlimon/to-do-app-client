88a36096ffc2ddb4402269ac698ec3683c974557
shahriarlimon <shahriarwebdev@gmail.com>
2022-05-18 20:49:24 +0600

    designed add to lists components

---
+++ src/Components/AddToLists/AddToLists.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const AddToLists = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const onSubmit = async (data) =>{
+
+  }
+  return (
+    <div className="flex items-center justify-center mt-12">
+      <div className="mt-5 ">
+        <h1 className="text-2xl font-bold">Add to To-Do Lists</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Task Title</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Task title"
+              class="input input-bordered w-full max-w-xs"
+              {...register("title", {
+                required: {
+                  value: true,
+                  message: "Title is required",
+                },
+              })}
+            />
+            <label class="label">
+              {errors.title?.type === "required" && (
+                <span class="label-text-alt text-red-500">
+                  {errors.title.message}
+                </span>
+              )}
+            </label>
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Task Description</span>
+            </label>
+            <textarea
+              type="text"
+              placeholder="Task description"
+              class="textarea w-full textarea-accent  max-w-xs"
+              {...register("description", {
+                required: {
+                  value: true,
+                  message: "Description is required",
+                },
+              })}
+            />
+            <label class="label">
+              {errors.description?.type === "required" && (
+                <span class="label-text-alt text-red-500">
+                  {errors.description.message}
+                </span>
+              )}
+            </label>
+          </div>
+
+          <input
+            className="btn btn-accent w-full max-w-xs text-white"
+            type="submit"
+            value="Add"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddToLists;
---
+++ src/Components/ToDo/ToDo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddToLists from '../AddToLists/AddToLists';
+
+const ToDo = () => {
+    return (
+        <div>
+            <AddToLists/>
+        </div>
+    );
+};
+
+export default ToDo;
